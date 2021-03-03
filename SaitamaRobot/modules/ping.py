86a2828a6feb6942fdc4e74e93b443cba25b9e6f
Sᴏɴ Gᴏᴋᴜ (孫悟空) <78256720+Telegram-Friend@users.noreply.github.com>
2021-03-03 19:28:42 +0530

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -80,8 +80,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "█▀█ █▀█ █▄░█ █▀▀ █ 
-         █▀▀ █▄█ █░▀█ █▄█ ▄\n"
+        "尸口𠘨厶\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML)
